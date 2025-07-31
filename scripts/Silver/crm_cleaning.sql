bf1160e635c15832555868185976b18b781a3bd2
Crowned-Sparrow <phanthiphuongnga1979@gmail.com>
2025-07-31 20:50:53 +0700

    Update crm_cleaning.sql

--- scripts/Silver/crm_cleaning.sql
+++ scripts/Silver/crm_cleaning.sql
@@ -84,6 +84,63 @@ END AS prd_line,
 CAST (prd_start_date AS DATE) AS prd_start_dt,
 CAST(LEAD(prd_start_date) OVER (PARTITION BY prd_key ORDER BY prd_start_date) -1 AS DATE) AS prd_end_dt
 FROM	bronze.crm_prd_info
+--Sales_info
+IF OBJECT_ID ('silver.crm_sales_details','U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details (
+	sls_ord_num		NVARCHAR(50),
+	sls_prd_key		NVARCHAR(50),	
+	sls_cust_id		INT,	
+	sls_order_dt	DATE,
+	sls_ship_dt		DATE,
+	sls_due_dt		DATE,
+	sls_sales		INT,
+	sls_quantity	INT,
+	sls_price		INT,
+	dwh_create_date		DATETIME2 DEFAULT GETDATE()
+);
+INSERT INTO silver.crm_sales_details(
+	sls_ord_num		,
+	sls_prd_key		,	
+	sls_cust_id		,	
+	sls_order_dt	,
+	sls_ship_dt		,
+	sls_due_dt		,
+	sls_sales		,
+	sls_quantity	,
+	sls_price		
+)
+SELECT sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+CASE 
+	WHEN sls_order_dt = 0 OR LEN(sls_order_dt) !=8 THEN NULL
+	ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+END AS sls_order_dt,
+CASE 
+	WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) !=8 THEN NULL
+	ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+END AS sls_ship_dt,
+CASE 
+	WHEN sls_due_dt = 0 OR LEN(sls_due_dt) !=8 THEN NULL
+	ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+END AS sls_due_dt,
+CASE
+	WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != ABS(sls_quantity*sls_price)
+	THEN ABS(sls_quantity*sls_price)
+	ELSE sls_sales
+END AS sls_sales,
+CASE 
+	WHEN sls_quantity IS NULL OR sls_quantity < 0
+	THEN sls_sales / NULLIF(sls_price,0)
+	ELSE sls_quantity	
+END AS sls_quantity,
+CASE 
+	WHEN sls_price IS NULL OR sls_price <= 0
+	THEN sls_sales / NULLIF(sls_quantity,0)
+	ELSE sls_price	
+END AS sls_price
+	FROM bronze.crm_sales_details
 
 
 
