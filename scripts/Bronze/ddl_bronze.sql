705febf04d19322a494a3f953dc628da17c913fc
Crowned-Sparrow <phanthiphuongnga1979@gmail.com>
2025-08-01 12:29:07 +0700

    Update ddl_bronze.sql

--- scripts/Bronze/ddl_bronze.sql
+++ scripts/Bronze/ddl_bronze.sql
@@ -1,3 +1,6 @@
+--NOTE--
+-- Running this will completely drop all tables and data containted
+
 IF OBJECT_ID ('bronze.crm_cust_info','U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
