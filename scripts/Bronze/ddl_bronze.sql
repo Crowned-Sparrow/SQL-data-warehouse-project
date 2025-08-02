a3f4b6a7764a24bfe2b9d5c01fbb82f58eca125b
Crowned-Sparrow <phanthiphuongnga1979@gmail.com>
2025-08-02 12:53:14 +0700

    Update ddl_bronze.sql

--- scripts/Bronze/ddl_bronze.sql
+++ scripts/Bronze/ddl_bronze.sql
@@ -17,8 +17,8 @@ CREATE TABLE bronze.crm_cust_info (
 IF OBJECT_ID ('bronze.crm_prd_info','U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info (
-	pdr_id			INT,
-	pdr_key			NVARCHAR(50),
+	prd_id			INT,
+	prd_key			NVARCHAR(50),
 	prd_nm			NVARCHAR(50),
 	prd_cost		INT,
 	prd_line		NVARCHAR(50),
